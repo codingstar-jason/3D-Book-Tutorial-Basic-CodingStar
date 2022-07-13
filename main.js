a47c4b942ca869ded0f0c68cee835e97e8300b0d
rrolonNelson <rrolon@nelson-atkins.org>
2022-07-13 13:25:17 -0500

    working on adding additional pages, need to work on next an prev function in js

--- main.js
+++ main.js
@@ -6,6 +6,43 @@ const book = document.querySelector("#book");
 const paper1 = document.querySelector("#p1");
 const paper2 = document.querySelector("#p2");
 const paper3 = document.querySelector("#p3");
+const paper4 = document.querySelector("#p4");
+const paper5 = document.querySelector("#p5");
+const paper6 = document.querySelector("#p6");
+const paper7 = document.querySelector("#p7");
+const paper8 = document.querySelector("#p8");
+const paper9 = document.querySelector("#p9");
+const paper10 = document.querySelector("#p10");
+const paper11 = document.querySelector("#p11");
+const paper12 = document.querySelector("#p12");
+const paper13 = document.querySelector("#p13");
+const paper14 = document.querySelector("#p14");
+const paper15 = document.querySelector("#p15");
+const paper16 = document.querySelector("#p16");
+const paper17 = document.querySelector("#p17");
+const paper18 = document.querySelector("#p18");
+const paper19 = document.querySelector("#p19");
+const paper20 = document.querySelector("#p20");
+const paper21 = document.querySelector("#p21");
+const paper22 = document.querySelector("#p22");
+const paper23 = document.querySelector("#p23");
+const paper24 = document.querySelector("#p24");
+const paper25 = document.querySelector("#p25");
+const paper26 = document.querySelector("#p26");
+const paper27 = document.querySelector("#p27");
+const paper28 = document.querySelector("#p28");
+const paper29 = document.querySelector("#p29");
+const paper30 = document.querySelector("#p30");
+const paper31 = document.querySelector("#p31");
+const paper32 = document.querySelector("#p32");
+const paper33 = document.querySelector("#p33");
+const paper34 = document.querySelector("#p34");
+const paper35 = document.querySelector("#p35");
+const paper36 = document.querySelector("#p36");
+const paper37 = document.querySelector("#p37");
+const paper38 = document.querySelector("#p38");
+
+
 
 // Event Listener
 prevBtn.addEventListener("click", goPrevPage);
@@ -13,7 +50,7 @@ nextBtn.addEventListener("click", goNextPage);
 
 // Business Logic
 let currentLocation = 1;
-let numOfPapers = 3;
+let numOfPapers = 38;
 let maxLocation = numOfPapers + 1;
 
 function openBook() {
@@ -50,6 +87,23 @@ function goNextPage() {
                 paper3.style.zIndex = 3;
                 closeBook(false);
                 break;
+            case 1:
+                openBook();
+                paper1.classList.add("flipped");
+                paper1.style.zIndex = 1;
+                break;
+            case 2:
+                paper2.classList.add("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 3:
+                paper3.classList.add("flipped");
+                paper3.style.zIndex = 3;
+                closeBook(false);
+                break;
+            
+
+            
             default:
                 throw new Error("unkown state");
         }
