43d6c960ebed4501bd268f9899bda386bc5d7134
rrolonNelson <rrolon@nelson-atkins.org>
2022-08-11 20:33:59 -0500

    added in page turning library that should work with touch, home button does not work now

---
+++ Img/book.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const pageFlip = new St.PageFlip(
+        document.getElementById("demoBookExample"),
+        {
+            width: 700, // base page width
+            height: 500, // base page height
+
+            size: "fixed",
+            // set threshold values:
+            // minWidth: 315,
+            // maxWidth: 1000,
+            // minHeight: 420,
+            // maxHeight: 1350,
+
+            maxShadowOpacity: 0.5, // Half shadow intensity
+            showCover: true,
+            mobileScrollSupport: false // disable content scrolling on mobile devices
+        }
+    );
+
+    // load pages
+    pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+
+    document.querySelector(".page-total").innerText = pageFlip.getPageCount();
+    document.querySelector(
+        ".page-orientation"
+    ).innerText = pageFlip.getOrientation();
+
+    document.querySelector(".btn-prev").addEventListener("click", () => {
+        pageFlip.flipPrev(); // Turn to the previous page (with animation)
+    });
+
+    document.querySelector(".btn-next").addEventListener("click", () => {
+        pageFlip.flipNext(); // Turn to the next page (with animation)
+    });
+
+    // triggered by page turning
+    pageFlip.on("flip", (e) => {
+        document.querySelector(".page-current").innerText = e.data + 1;
+    });
+
+    // triggered when the state of the book changes
+    pageFlip.on("changeState", (e) => {
+        document.querySelector(".page-state").innerText = e.data;
+    });
+
+    // triggered when page orientation changes
+    pageFlip.on("changeOrientation", (e) => {
+        document.querySelector(".page-orientation").innerText = e.data;
+    });
+});
